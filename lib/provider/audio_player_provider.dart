7ea91e20d9121580e8cf181bf76ddf3d68649d1e
famio <smartboy.famio@gmail.com>
2022-07-18 22:36:33 +0900

    再生機能まで実装

--- lib/provider/audio_player_provider.dart
+++ lib/provider/audio_player_provider.dart
@@ -1,3 +1,46 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fudiko/provider/auth_provider.dart';
+import 'package:fudiko/provider/play_controller_provider.dart';
+import 'package:fudiko/provider/selected_station_stream_url_provider.dart';
 import 'package:just_audio/just_audio.dart';
 
-final audioPlayer = AudioPlayer();
+final audioPlayerProvier = Provider((ref) {
+  final audioPlayer = AudioPlayer();
+
+  ref.listen<AsyncValue<String?>>(selectedStationStreamUrlProvider,
+      (previous, next) {
+    next.when(
+        data: (data) async {
+          if (data == null) return;
+          ref.read(authProvider).whenData((authInfo) async {
+            final Map<String, String> header = {
+              "X-Radiko-Authtoken": authInfo.authToken,
+            };
+            try {
+              await audioPlayer.setUrl(data, headers: header);
+            } catch (_) {}
+            if (ref.read(isPlayingProvider) && !audioPlayer.playing) {
+              try {
+                await audioPlayer.play();
+              } catch (_) {}
+            }
+          });
+        },
+        error: (error, trace) {},
+        loading: () {});
+  });
+  ref.listen<bool>(isPlayingProvider, (previous, next) async {
+    if (next) {
+      ref.read(selectedStationStreamUrlProvider).whenData((value) async {
+        if (value == null) return;
+        try {
+          await audioPlayer.play();
+        } catch (_) {}
+      });
+    } else {
+      try {
+        await audioPlayer.stop();
+      } catch (_) {}
+    }
+  });
+});
--- lib/ui/main/play_controller.dart
+++ lib/ui/main/play_controller.dart
@@ -6,32 +6,26 @@ class _PlayController extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isPlaying = ref.watch(isPlayingProvider);
-    return Row(children: [
-      InkWell(
-          child: Icon(
-            isPlaying ? Icons.pause : Icons.play_arrow,
-            size: 50,
-          ),
-          onTap: () {
-            ref.read(isPlayingProvider.notifier).state ^= true;
-            if (audioPlayer.playing) {
-              audioPlayer.pause();
-            } else {
-              final asyncurl = ref.read(selectedStationStreamUrlProvider);
-              print('async url:${asyncurl}');
-              asyncurl.whenData((url) {
-                print('url:${url}');
-                if (url == null) return;
-                ref.read(authProvider).whenData((authinfo) {
-                  final Map<String, String> header = {
-                    "X-Radiko-Authtoken": authinfo.authToken
-                  };
-                  audioPlayer.setUrl(url, headers: header);
-                  audioPlayer.play();
-                });
-              });
-            }
-          })
-    ]);
+    final selectedProgram = ref.watch(selectedStationOnAirProgramProvider);
+    final _ = ref.read(audioPlayerProvier);
+    return SafeArea(
+      child: Row(children: [
+        const SizedBox(width: 8),
+        InkWell(
+            child: Icon(
+              isPlaying ? Icons.pause : Icons.play_arrow,
+              size: 50,
+            ),
+            onTap: () {
+              ref.read(isPlayingProvider.notifier).state ^= true;
+            }),
+        const SizedBox(width: 8),
+        Text(
+          selectedProgram.maybeWhen(
+              data: (data) => data == null ? '' : data.title, orElse: () => ''),
+          style: const TextStyle(fontSize: 20),
+        )
+      ]),
+    );
   }
 }
