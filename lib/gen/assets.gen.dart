5f582012fb6782c4fccf3203c43dfa62081a3ff8
famio <smartboy.famio@gmail.com>
2023-04-16 16:44:30 +0900

    Flutter SDK 3.7.11

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -3,13 +3,13 @@
 ///  FlutterGen
 /// *****************************************************
 
+import 'package:flutter/services.dart';
 // coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter/services.dart';
 
 class $AssetsImagesGen {
   const $AssetsImagesGen();
@@ -138,7 +138,7 @@ class SvgGenImage {
       colorFilter: colorFilter,
       color: color,
       colorBlendMode: colorBlendMode,
-      clipBehavior: clipBehavior,
+      clipBehavior: clipBehavior!,
       cacheColorFilter: cacheColorFilter,
     );
   }
